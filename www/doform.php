4c0df5643d7cc52d47a70d68be030e17516bd40c
Tyler Ramey <tyramey95@gmail.com>
2023-04-19 15:00:33 -0400

    Added indents for readability.

--- www/doform.php
+++ www/doform.php
@@ -1,9 +1,9 @@
 <html>
-<head><title>Processing Form</title></head>
-<body>
-<?php
-echo 'You entered: '.$_POST['name'].'<br>';
-echo 'Hidden value: '.$_POST['hiddenvalue'];
-?>
-</body>
+    <head><title>Processing Form</title></head>
+    <body>
+        <?php
+        echo 'You entered: '.$_POST['name'].'<br>';
+        echo 'Hidden value: '.$_POST['hiddenvalue'];
+        ?>
+    </body>
 </html>
--- www/index.php
+++ www/index.php
@@ -1,10 +1,10 @@
 <html>
-<head><title>A Form</title></head>
-<body>
-<form action="doform.php" method="POST">
-<input type="hidden" name="hiddenvalue" value="foo">
-Name: <input type="text" name="name">
-<input type="submit" value="Submit">
-</form>
-</body>
+    <head><title>A Form</title></head>
+    <body>
+        <form action="doform.php" method="POST">
+            <input type="hidden" name="hiddenvalue" value="foo">
+            Name: <input type="text" name="name">
+            <input type="submit" value="Submit">
+        </form>
+    </body>
 </html>
