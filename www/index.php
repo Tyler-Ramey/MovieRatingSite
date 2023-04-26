9915cd4e65c161cb07b152379929318ab27c142b
Tyler Ramey <tyramey95@gmail.com>
2023-04-26 12:26:30 -0400

    Added include for header, footer, friends, and activity. Fixed CSS not loading.

--- www/index.php
+++ www/index.php
@@ -4,55 +4,26 @@
 	<title>Movie Site</title>
 
     <?php
-    require('config.php');
-    include_once($includePath\styles.css);
+    // require('config.php');
+    include_once('..\config\styles.css');
     ?>
 </head>
 <body>
-	<header class="header">
-		<div class="logo">
-			<img src="https://www.w3schools.com/images/w3schools_green.jpg" alt="Movie Site Logo">
-		</div>
-        <div class="search-container">
-          <input class="search" type="text" placeholder="Actors, Movies, Users...">
-          <button class="search-button">&#10148;</button>
-        </div>
-		<div class="login">
-			<button>Login / Register</button>
-		</div>
-	</header>
+	<?php require('..\config\header.php'); ?>
 	<main class="main">
 		<div class="left-sidebar">
-			<div class="friends-box">
-				<h2>Friends</h2>
-				<p>John Doe</p>
-				<p>Jane Smith</p>
-                <p>John Doe</p>
-				<p>Jane Smith</p>
-                <p>John Doe</p>
-				<p>Jane Smith</p>
-                <p>John Doe</p>
-				<p>Jane Smith</p>
-                <p>John Doe</p>
-				<p>Jane Smith</p>
-                <p>John Doe</p>
-				<p>Jane Smith</p>
-			</div>
+            <?php require('..\config\friends.php'); ?>
 		</div>
 		<div class="content">
 			<h2>Dynamic Content Goes Here</h2>
 			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pulvinar est at euismod efficitur. Aliquam eu arcu leo. Nulla facilisi. Etiam rutrum urna quis eros suscipit, in sodales ipsum pretium. Fusce ac elit elit. Maecenas luctus ipsum sed ex posuere, non fringilla lorem imperdiet. Curabitur eget massa vel velit rhoncus egestas vel sit amet lectus. Sed euismod justo vel turpis facilisis interdum.</p>
 		</div>
 		<div class="right-sidebar">
-			<div class="activity-box">
-				<h2>Activity</h2>
-				<p>Star Wars was added by John Doe</p>
-				<p>Jane Smith liked The Matrix</p>
-			</div>
+            <?php require('..\config\activity.php'); ?>
 		</div>
 	</main>
-	<footer class="footer">
-		Designed by Tyler Ramey for CPSC 365 - Muskingum University
-	</footer>
+
+    <?php require('..\config\footer.php'); ?>
+
 </body>
 </html>
