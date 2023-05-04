844a08e09052e48eb0b78063f1713f45ed2cb4e5
Tyler Ramey <tyramey95@gmail.com>
2023-05-04 12:44:37 -0400

    Added admin page link to footer

--- config/footer.php
+++ config/footer.php
@@ -1,3 +1,9 @@
 <footer class="footer">
-		Designed by Tyler Ramey for CPSC 365 - Muskingum University
-	</footer>
+    <h4>Designed by Tyler Ramey for CPSC 365 - Muskingum University</h4>
+    <?php
+    session_start();
+    if ($_SESSION['admin'] == 1) {
+        echo '<a href="admin.php">Admin Page</a>';
+    }
+    ?>
+</footer>
