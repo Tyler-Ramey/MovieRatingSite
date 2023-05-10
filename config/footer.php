78a5bc79ce675f6d9aa4cb57425e83451cf6462c
Tyler Ramey <tyramey95@gmail.com>
2023-05-10 12:07:21 -0400

    Added link to home page and CSS for links

--- config/footer.php
+++ config/footer.php
@@ -1,9 +1,12 @@
 <footer class="footer">
     <h4>Designed by Tyler Ramey for CPSC 365 - Muskingum University</h4>
-    <?php
-    session_start();
-    if ($_SESSION['admin'] == 1) {
-        echo '<a href="admin.php">Admin Page</a>';
-    }
-    ?>
+    <div class="nav-links">
+        <?php
+        session_start();
+        echo '<a href="index.php">Home</a>';
+        if ($_SESSION['admin'] == 1) {
+            echo '<a href="admin.php">Admin Page</a>';
+        }
+        ?>
+    </div>
 </footer>
