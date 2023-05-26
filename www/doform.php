67f0cb8379ac7f1991b540b179ddf92ad49674aa
Tyler Ramey <tyramey95@gmail.com>
2023-05-26 15:15:27 -0400

    Unused file. Removed.

--- www/doform.php
+++
@@ -1,9 +0,0 @@
-<html>
-    <head><title>Processing Form</title></head>
-    <body>
-        <?php
-        echo 'You entered: '.$_POST['name'].'<br>';
-        echo 'Hidden value: '.$_POST['hiddenvalue'];
-        ?>
-    </body>
-</html>
