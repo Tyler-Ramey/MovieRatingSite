64f4d3bcf85b4d9d524163d88775a493b5246401
Tyler Ramey <tyramey95@gmail.com>
2023-05-29 21:08:10 -0400

    Added lockout and time delay after 3 failed attempts at password

--- www/login-user.php
+++ www/login-user.php
@@ -1,37 +1,77 @@
 <?php
+session_start();
+
 require_once('..\config\dbconnect.php');
 
-$username = trim($_POST['username']);
-$password = $_POST['password'];
+$maxFailedAttempts = 3;
+$lockoutDuration = 60; // in seconds (1 minute)
 
-$sql = 'SELECT username, password, admin FROM People WHERE username = :username';
-$stmt = $pdo->prepare($sql);
-$stmt->bindParam(':username', $username);
-$stmt->execute();
-$result = $stmt->fetch();
-
-if (!$result) {
-    //User not found
-    $error = "Invalid username or password";
-    echo "<p>" . $error . "</p>";
-    header("Refresh:5; url=login.php");
-    exit();
+// Check if the user has exceeded the maximum failed attempts
+if (isset($_SESSION['failedAttempts']) && $_SESSION['failedAttempts'] >= $maxFailedAttempts) {
+    // Check if the lockout duration has passed
+    if (isset($_SESSION['lockoutTime']) && (time() - $_SESSION['lockoutTime']) < $lockoutDuration) {
+        // Account is locked, display an error message
+        echo "<p>Account locked. Please try again later.</p>";
+        header("Refresh: 5; url: login.php");
+    } else {
+        // Lockout duration has passed, reset the failed attempts
+        $_SESSION['failedAttempts'] = 0;
+        unset($_SESSION['lockoutTime']);
+    }
 }
 
-$encryptedPassword = $result['password'];
+// Validate login credentials
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = trim($_POST['username']);
+    $password = $_POST['password'];
 
-if (password_verify($password, $encryptedPassword)) {
-    // Passwords match
-    session_start();
-    session_regenerate_id(true);
-    $_SESSION['username'] = $username;
-    $_SESSION['admin'] = $result['admin'];
-    header("Location: index.php");
-    exit();
-} else {
-    // Passwords don't match
-    $error = "Invalid username or password";
-    echo "<p>" . $error . "</p>";
-    header("Refresh:5; url=login.php");
+    // Query the database to check the credentials
+    $sql = 'SELECT username, password, admin FROM People WHERE username = :username';
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(':username', $username);
+    $stmt->execute();
+    $result = $stmt->fetch();
+
+    if (!$result) {
+        // User not found
+        $_SESSION['failedAttempts'] = isset($_SESSION['failedAttempts']) ? $_SESSION['failedAttempts'] + 1 : 1;
+
+        // Check if the maximum failed attempts have been reached
+        if ($_SESSION['failedAttempts'] >= $maxFailedAttempts) {
+            // Set the lockout time
+            $_SESSION['lockoutTime'] = time();
+            echo "<p>Invalid username or password. Account locked. Please try again later.</p>";
+        } else {
+            echo "<p>Invalid username or password</p>";
+        }
+
+        header("Refresh: 5; url=login.php");
+        exit();
+    }
+
+    $encryptedPassword = $result['password'];
+
+    if (password_verify($password, $encryptedPassword)) {
+        // Passwords match
+        $_SESSION['username'] = $username;
+        $_SESSION['admin'] = $result['admin'];
+        header("Location: index.php");
+        exit();
+    } else {
+        // Passwords don't match
+        $_SESSION['failedAttempts'] = isset($_SESSION['failedAttempts']) ? $_SESSION['failedAttempts'] + 1 : 1;
+
+        // Check if the maximum failed attempts have been reached
+        if ($_SESSION['failedAttempts'] >= $maxFailedAttempts) {
+            // Set the lockout time
+            $_SESSION['lockoutTime'] = time();
+            echo "<p>Invalid username or password. Account locked. Please try again later.</p>";
+        } else {
+            echo "<p>Invalid username or password</p>";
+        }
+
+        header("Refresh: 5; url=login.php");
+        exit();
+    }
 }
 ?>
