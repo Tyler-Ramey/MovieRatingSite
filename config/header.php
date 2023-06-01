1d3b1d000578244393071fd445a111e43613a82a
Tyler Ramey <tyramey95@gmail.com>
2023-06-01 14:41:46 -0400

    Updated header to include working search bar. Can search for users and movies

--- config/header.php
+++ config/header.php
@@ -3,10 +3,12 @@
     <div class="logo">
         <img src="logo.png" alt="Movie Site Logo">
     </div>
-    <div class="search-container">
-        <input class="search" type="text" placeholder="Actors, Movies, Users...">
-        <button class="search-button">&#10148;</button>
-    </div>
+    <form class="search-form" action="..\www\search.php" method="GET">
+        <div class="search-container">
+            <input class="search" type="text" name="query" placeholder="Search Movies and Users...">
+            <button class="search-button" type="submit">&#10148;</button>
+        </div>
+    </form>
     <div class="login">
         <?php
         if (isset($_SESSION['username'])) {
