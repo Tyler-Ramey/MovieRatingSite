65b2f985974a850d143e71c8146cfb622aa30507
Tyler Ramey <tyramey95@gmail.com>
2023-06-02 16:20:22 -0400

    Fixed errors with add-movie page.

--- www/add-movie.php
+++ www/add-movie.php
@@ -2,9 +2,12 @@
 require_once('../config/dbconnect.php');
 require_once('image-upload.php');
 
-$title = $_POST['Title'];
-$releaseDate = $_POST['ReleaseDate'];
-$summary = $_POST['Summary'];
+$title = $_POST['title'];
+$releaseDate = $_POST['release-date'];
+$summary = $_POST['summary'];
+
+//Upload the image
+$imagePath = uploadImage($title, $_FILES['image'],);
 
 // Insert the movie into the database with the image path
 $sql = 'INSERT INTO movies (Title, ReleaseDate, Summary, ImagePath) VALUES (:title, :releaseDate, :summary, :imagePath)';
@@ -18,7 +21,4 @@ $stmt->execute();
 // Redirect to the admin page after adding the movie
 header('Location: admin.php');
 exit();
-} else {
-    die('Error: File upload failed. Please try again.');
-}
 ?>
