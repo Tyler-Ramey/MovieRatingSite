a5fec20a748932d2d817b016fb56f1c024a1ac6e
Tyler Ramey <tyramey95@gmail.com>
2023-06-02 00:06:08 -0400

    Changed header to include clickable link and updated CSS

--- config/header.php
+++ config/header.php
@@ -3,7 +3,7 @@
     <div class="logo">
         <img src="logo.png" alt="Movie Site Logo">
     </div>
-    <form class="search-form" action="..\www\search.php" method="GET">
+    <form class="search-form" action="../www/search.php" method="GET">
         <div class="search-container">
             <input class="search" type="text" name="query" placeholder="Search Movies and Users...">
             <button class="search-button" type="submit">&#10148;</button>
@@ -12,8 +12,9 @@
     <div class="login">
         <?php
         if (isset($_SESSION['username'])) {
-            echo '<h2>' . $_SESSION['username'] . '</h2><br>';
-            echo '<a href="logout.php" class="logout-button style="color: blue;">Logout</a>';
+            $username = $_SESSION['username'];
+            echo '<h3 class="username"><a href="user.php?username=' . $username . '">' . $username . '</a></h3>';
+            echo '<a href="logout.php" class="logout-button">Logout</a>';
         } else {
             echo '<a href="login.php" class="login-button">Login / Register</a>';
         }
